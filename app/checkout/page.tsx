3479e6754cc8fcb41902d83497e89246120003be
topcelltaquarituba <topcelltaquarituba@gmail.com>
2025-10-27 17:29:33 -0300

    error fix: checkoutPage

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useCart } from '@/context/CartContext'
 import { useRouter } from 'next/navigation'
 import CEPInput, { CEPData } from '@/components/CEPInput'
 import { FiCheck, FiCreditCard, FiTruck, FiLock, FiMapPin } from 'react-icons/fi'
 
 export default function CheckoutPage() {
+  const [path, setPath] = useState("")
+  useEffect(() => {
+    setPath(window.location.pathname)
+  }, [])
+  return(
+    <div>Você está em: {path}</div>
+  )
   const { items, getTotalPrice, clearCart } = useCart()
   const router = useRouter()
   const [step, setStep] = useState<'shipping' | 'payment' | 'review'>('shipping')
