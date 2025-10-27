703ca9a9c4fd00c695a21211ecbfb1115ef639fa
topcelltaquarituba <topcelltaquarituba@gmail.com>
2025-10-27 15:56:47 -0300

    fix: erro compilação

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { getProductById, Review } from '@/lib/products'
 import { useCart } from '@/context/CartContext'
